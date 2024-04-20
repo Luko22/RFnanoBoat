0c6d0bd8db06e4331d09c4fe38f16796d0d209b7
Anirudh Madhusudhan <anirudhmadhusudhan@Anirudhs-MacBook-Air.fritz.box>
2024-04-20 17:20:28 +0200

    made code modular

--- BoatReceiver/BoatReceiver.ino
+++ BoatReceiver/BoatReceiver.ino
@@ -1,81 +1,94 @@
-//https://www.fambach.net/rf-nano/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-RF24 radio(10, 9); // CE, CSN
-const byte address[6] = "00001"; // the address the the module
-///pins D10,D9,D13,D4,D5 : DO NOT USE THESE PINS
 #include <Servo.h>
-#define ESC1_PIN 6
-#define ESC2_PIN 5
-Servo esc1;
-Servo esc2;
-Servo fin;
 
+// Pin definitions
+const uint8_t CE_PIN = 10;
+const uint8_t CSN_PIN = 9;
+const uint8_t ESC1_PIN = 6;
+const uint8_t ESC2_PIN = 5;
+const uint8_t FIN_PIN = 3;
+
+// Radio and address
+RF24 radio(CE_PIN, CSN_PIN);
+const byte address[6] = "00001";
+
+// Servo objects
+Servo esc1, esc2, fin;
+
+// Initialization of components
 void setup() {
   Serial.begin(9600);
+  initializeRadio();
+  initializeServos();
+}
 
+// Main loop listening for RF data
+void loop() {
+  processReceivedData();
+}
+
+// Initialize the NRF24L01 radio
+void initializeRadio() {
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MAX);
   radio.startListening();
-
-  fin.attach(3);
-  for(int i=180;i>0;i--){
-    fin.write(i);
-    delay(10);
-  }
-  for(int i=0;i<180;i++){
-    fin.write(i);
-    delay(10);
-  }
-
-  esc1.attach(ESC1_PIN, 1000, 2000);
-  esc1.write(180);
-  delay(5000);
-  esc1.write(0);
-  delay(2000);
-  esc1.write(10);  
-
-  esc2.attach(ESC2_PIN, 1000, 2000);
-  esc2.write(180);
-  delay(5000);
-  esc2.write(0);
-  delay(2000);
-  esc2.write(10);
-
-  
 }
 
-void loop() {
+// Attach servos and move fin for setup
+void initializeServos() {
+  fin.attach(FIN_PIN);
+  cycleFin();
+
+  esc1.attach(ESC1_PIN, 1000, 2000);
+  initializeESC(esc1);
+
+  esc2.attach(ESC2_PIN, 1000, 2000);
+  initializeESC(esc2);
+}
+
+// Simple sweep for fin initialization
+void cycleFin() {
+  for (int pos = 180; pos >= 0; pos--) {
+    fin.write(pos);
+    delay(10);
+  }
+  for (int pos = 0; pos <= 180; pos++) {
+    fin.write(pos);
+    delay(10);
+  }
+}
+
+// General ESC initialization sequence
+void initializeESC(Servo &esc) {
+  esc.write(180);
+  delay(5000);
+  esc.write(0);
+  delay(2000);
+  esc.write(10);
+}
+
+// Process received data and control servos
+void processReceivedData() {
   if (radio.available()) {
     int receivedData[2];
     radio.read(&receivedData, sizeof(receivedData));
-
-    int throttleCW = receivedData[0];
-    int throttleCCW = receivedData[1];
-
-    int motorCW = map(throttleCW, 300, 0, 0, 180);
-    int motorCCW = map(throttleCCW, 300, 0, 0, 180);
-    int rudder;
-
-    // if (motorCW>(motorCCW*2)) rudder= map(motorCW, 0, 180, 90, 135);
-    // else if(motorCCW>(motorCW*2)) rudder= map(motorCCW, 0, 180, 90, 45);
-    // else rudder=90;
-
-    // Control the motors
-    esc1.write(motorCW);
-    esc2.write(motorCCW);
-    // fin.write(rudder);
-
-    Serial.print(throttleCW);
-    Serial.print("  |  ");
-    Serial.print(motorCW);
-    Serial.print("  |  ");
-    Serial.print(throttleCCW);
-    Serial.print("  |  ");
-    Serial.println(motorCCW);
+    controlMotors(receivedData);
   }
-
 }
+
+// Map and write control values to ESCs
+void controlMotors(const int *data) {
+  int motorCW = map(data[0], 300, 0, 0, 180);
+  int motorCCW = map(data[1], 300, 0, 0, 180);
+
+  esc1.write(motorCW);
+  esc2.write(motorCCW);
+
+  // Debugging output
+  Serial.print("CW: "); Serial.print(data[0]); Serial.print(" -> "); Serial.print(motorCW);
+  Serial.print(" | CCW: "); Serial.print(data[1]); Serial.print(" -> "); Serial.println(motorCCW);
+}
+
--- RFNanoRemote/RFNanoRemote.ino
+++ RFNanoRemote/RFNanoRemote.ino
@@ -1,41 +1,50 @@
-// Arduino + nrf24L01+ module transmitter
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(10, 9); // CE, CSN
-const byte address[6] = "00001"; // the address the the module
-///pins 10,9,13,4,5 DO NOT USE
-#define JoyStick_X_PIN A0
-#define JoyStick_Y_PIN A1
-int joyposVert;
-int joyposHorz;
+// Pin assignments
+const uint8_t CE_PIN = 10;
+const uint8_t CSN_PIN = 9;
+const uint8_t JOYSTICK_X_PIN = A0;
+const uint8_t JOYSTICK_Y_PIN = A1;
+
+// RF24 Radio setup
+RF24 radio(CE_PIN, CSN_PIN);
+const byte address[6] = "00001";
+
+// Structure to hold joystick data
+struct JoystickData {
+  int vertical;
+  int horizontal;
+} joystickData;
 
 void setup() {
   Serial.begin(9600);
-
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MAX);
   radio.stopListening();
-  
-  pinMode(JoyStick_X_PIN, INPUT);
-  pinMode(JoyStick_Y_PIN, INPUT);
+
+  pinMode(JOYSTICK_X_PIN, INPUT);
+  pinMode(JOYSTICK_Y_PIN, INPUT);
 }
 
 void loop() {
-  int joyposVert = analogRead(JoyStick_X_PIN);
-  int joyposHorz = analogRead(JoyStick_Y_PIN);
+  readJoystick();
+  sendJoystickData();
+  delay(50); // Delay to avoid overwhelming the radio
+}
 
-  Serial.print(joyposVert);
-  Serial.print("  |  ");
-  Serial.println(joyposHorz);
+void readJoystick() {
+  joystickData.vertical = analogRead(JOYSTICK_X_PIN);
+  joystickData.horizontal = analogRead(JOYSTICK_Y_PIN);
+  Serial.print("Vertical: ");
+  Serial.print(joystickData.vertical);
+  Serial.print(" | Horizontal: ");
+  Serial.println(joystickData.horizontal);
+}
 
-  int joystickData[2] = {joyposVert, joyposHorz};
-
+void sendJoystickData() {
   // Send joystick data over NRF24L01+
   radio.write(&joystickData, sizeof(joystickData));
-
-  delay(50); // Add a delay to avoid overwhelming the radio
 }
